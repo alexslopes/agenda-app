c81965f21607fb78b764a05d9c1a64394d25f14c
alexs <alexstlopes@gmail.com>
2022-07-17 18:02:10 -0300

    143. Consulta Paginada com Angular Material

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -6,6 +6,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 
 import { ContatoDetalheComponent } from '../contato-detalhe/contato-detalhe.component'
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-contato',
@@ -17,6 +18,10 @@ export class ContatoComponent implements OnInit {
   formulario: FormGroup;
   contatos: Contato[] = [];
   colunas = ['foto', 'id', 'nome', 'email', 'favorito'];
+  totalElementos = 0;
+  pagina = 0;
+  tamanho = 5;
+  pageSizeOptions: number[] = [5]
 
   constructor(
     private service: ContatoService,
@@ -29,7 +34,7 @@ export class ContatoComponent implements OnInit {
 
   ngOnInit(): void {
     this.montarFormulario();
-    this.listarContatos();
+    this.listarContatos(this.pagina, this.tamanho);
   }
 
   montarFormulario(){
@@ -39,9 +44,11 @@ export class ContatoComponent implements OnInit {
     })
   }
 
-  listarContatos(){
-    this.service.list().subscribe(response =>  {
-      this.contatos = response;
+  listarContatos(pagina = 0, tamanho = 0){
+    this.service.list(pagina, tamanho).subscribe(response =>  {
+      this.contatos = response.content;
+      this.totalElementos = response.totalElements;
+      this.pagina = response.number;
     })
   }
 
@@ -77,6 +84,11 @@ export class ContatoComponent implements OnInit {
       height: '450px',
       data: contato
     })
+  }
+
+  paginar(event: PageEvent){
+    this.pagina = event.pageIndex;
+    this.listarContatos(this.pagina, this.tamanho);
   }
 
 }
